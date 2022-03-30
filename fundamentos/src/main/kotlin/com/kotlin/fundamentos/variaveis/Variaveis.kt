a060e86a4b1f672493a61594d51672dcd5461487
jldv <jardel.gomes@gft.com>
2022-03-30 17:45:28 -0300

    Tipos variaveis

---
+++ fundamentos/src/main/kotlin/com/kotlin/fundamentos/variaveis/Variaveis.kt
@@ -0,0 +1,8 @@
+package com.kotlin.fundamentos.variaveis
+
+fun main(){
+    var nome = "Maria"//variavel mutavel
+    val nomeVal = "Maria"//variavel imutavel
+    nome = "Antonio"
+  //  nomeVal = "Antonio"
+}
