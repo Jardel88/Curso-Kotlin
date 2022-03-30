3cce5d2d61f7136bbdbf747966d5975564661e50
jldv <jardel.gomes@gft.com>
2022-03-30 14:34:47 -0300

    Variaveis mutaveis

--- fundamentos/src/main/kotlin/com/kotlin/fundamentos/atributos/Carro.kt
+++ fundamentos/src/main/kotlin/com/kotlin/fundamentos/atributos/Carro.kt
@@ -1,9 +1,11 @@
 package com.kotlin.fundamentos.atributos
 
-class Carro(val cor: String, val anoFabricacao: Int) {
+class Carro(var cor: String, var anoFabricacao: Int) {
 }
 
 fun main(){
     var carro = Carro("Preto", 2022)
+    carro.cor = "Branco"
+    carro.anoFabricacao = 2021
     println("Cor: " + carro.cor +", Ano de fabricação: "+ carro.anoFabricacao)
 }
