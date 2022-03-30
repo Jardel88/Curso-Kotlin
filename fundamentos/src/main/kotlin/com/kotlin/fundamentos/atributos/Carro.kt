df7240a3cb692ac12f4cdbee728b50ad073d1945
jldv <jardel.gomes@gft.com>
2022-03-30 14:47:19 -0300

    Classe dentro de Classe

--- fundamentos/src/main/kotlin/com/kotlin/fundamentos/atributos/Carro.kt
+++ fundamentos/src/main/kotlin/com/kotlin/fundamentos/atributos/Carro.kt
@@ -1,11 +1,14 @@
 package com.kotlin.fundamentos.atributos
 
-class Carro(var cor: String, var anoFabricacao: Int) {
+class Carro(var cor: String, var anoFabricacao: Int, val dono: Proprietario) {
+}
+class Proprietario(var nome: String, var idade: Int){
 }
 
 fun main(){
-    var carro = Carro("Preto", 2022)
+    var carro = Carro("Preto", 2022, Proprietario("Antonio", 30))
     carro.cor = "Branco"
     carro.anoFabricacao = 2021
     println("Cor: " + carro.cor +", Ano de fabricação: "+ carro.anoFabricacao)
+    println("Proprietario: " + carro.dono.nome +", Idade: "+ carro.dono.idade)
 }
