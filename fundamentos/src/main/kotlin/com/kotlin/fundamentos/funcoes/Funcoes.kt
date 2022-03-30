b4c6acd0cbc684ab443092cf88385b12a5bb78cf
jldv <jardel.gomes@gft.com>
2022-03-30 17:31:01 -0300

    funcoes com parametros

--- fundamentos/src/main/kotlin/com/kotlin/fundamentos/funcoes/Funcoes.kt
+++ fundamentos/src/main/kotlin/com/kotlin/fundamentos/funcoes/Funcoes.kt
@@ -1,12 +1,12 @@
 package com.kotlin.fundamentos.funcoes
 
 fun main(){
-    var nome = retornaNome()
-    println("Hell World")
-    println(retornaNome())
-    println(nome)
+    dizOi("Maria", 30)
 }
 
 fun retornaNome(): String {
     return "Antonio"
+}
+fun dizOi(nome: String, idade: Int){
+    println("Oi ${nome}, sua idade é: ${idade}")
 }
