7514f9c760236cd4fa433afda9af83d3265d5a41
jldv <jardel.gomes@gft.com>
2022-04-04 20:10:11 -0300

    Alterando Status code

--- mercadolivro/src/main/kotlin/com/kotlin/mercadolivro/controllers/CustomerController.kt
+++ mercadolivro/src/main/kotlin/com/kotlin/mercadolivro/controllers/CustomerController.kt
@@ -2,10 +2,12 @@ package com.kotlin.mercadolivro.controllers
 
 import com.kotlin.mercadolivro.controllers.request.PostCustomerRequest
 import com.kotlin.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -17,6 +19,7 @@ class CustomerController {
         return CustomerModel("1", "Maria", "maria@email.com")
     }
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     fun create(@RequestBody customer: PostCustomerRequest){
         println(customer)
     }
