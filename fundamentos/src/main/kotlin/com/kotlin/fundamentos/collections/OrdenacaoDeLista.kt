64f2394c9da84834c5fa5c451cd5ac9cb01c56f6
jldv <jardel.gomes@gft.com>
2022-04-04 12:05:41 -0300

    Collections parte 2

---
+++ fundamentos/src/main/kotlin/com/kotlin/fundamentos/collections/OrdenacaoDeLista.kt
@@ -0,0 +1,10 @@
+package com.kotlin.fundamentos.collections
+
+fun main(){
+    var lista = mutableListOf(9,8,7,6,5,4,3,2,1)
+    println(lista)
+    lista.sort()
+    println(lista)
+    lista.shuffle()
+    println(lista)
+}
