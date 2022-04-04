4aac0ae7b63171dc870b734d1ccf0ab5cc5ea3e8
jldv <jardel.gomes@gft.com>
2022-04-04 19:19:44 -0300

    Model e endpoint get

---
+++ mercadolivro/src/main/kotlin/com/kotlin/mercadolivro/controllers/CustomerController.kt
@@ -0,0 +1,16 @@
+package com.kotlin.mercadolivro.controllers
+
+import com.kotlin.mercadolivro.model.CustomerModel
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("customer")
+class CustomerController {
+
+    @GetMapping
+    fun getCustomer(): CustomerModel{
+        return CustomerModel("1", "Maria", "maria@email.com")
+    }
+}
---
+++ mercadolivro/src/main/kotlin/com/kotlin/mercadolivro/model/CustomerModel.kt
@@ -0,0 +1,7 @@
+package com.kotlin.mercadolivro.model
+
+data class CustomerModel(
+    var id: String,
+    var name: String,
+    var email: String
+)
