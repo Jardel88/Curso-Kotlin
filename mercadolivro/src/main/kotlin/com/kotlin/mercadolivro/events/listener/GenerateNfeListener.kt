03b98e63a81f0c7d1ccc6b4793bc26ded45de4a6
jldv <jardel.gomes@gft.com>
2022-04-19 16:14:38 -0300

    Listener do evento

---
+++ mercadolivro/src/main/kotlin/com/kotlin/mercadolivro/events/listener/GenerateNfeListener.kt
@@ -0,0 +1,20 @@
+package com.kotlin.mercadolivro.events.listener
+
+import com.kotlin.mercadolivro.events.PurchaseEvent
+import com.kotlin.mercadolivro.services.PurchaseService
+import org.springframework.context.event.EventListener
+import org.springframework.stereotype.Component
+import java.util.*
+
+@Component
+class GenerateNfeListener(
+    private val purchaseService: PurchaseService
+) {
+
+    @EventListener
+    fun listen(purchaseEvent: PurchaseEvent){
+        val nfe = UUID.randomUUID().toString()
+        val purchaseModel = purchaseEvent.purchaseModel.copy(nfe = nfe)
+        purchaseService.update(purchaseModel)
+    }
+}
