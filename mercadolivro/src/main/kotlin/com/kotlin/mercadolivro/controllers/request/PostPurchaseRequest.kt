1b9cc0b8ba8c82a407f9ef3eb83d1879b5457ba7
jldv <jardel.gomes@gft.com>
2022-04-19 11:41:07 -0300

    Entidade compra e migration

---
+++ mercadolivro/src/main/kotlin/com/kotlin/mercadolivro/controllers/request/PostPurchaseRequest.kt
@@ -0,0 +1,15 @@
+package com.kotlin.mercadolivro.controllers.request
+
+import javax.validation.constraints.NotNull
+import javax.validation.constraints.Positive
+
+data class PostPurchaseRequest(
+
+    @field:NotNull
+    @field:Positive
+    val customerId: Int,
+
+    @field:NotNull
+    val bookId: Set<Int>
+
+)
